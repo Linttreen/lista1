31f8ae55a67f8a8784e601d18bba937741b0c977
Linttreen <150390683+Linttreen@users.noreply.github.com>
2024-08-05 09:51:57 -0300

    Update MRU.java

--- src/MRU.java
+++ src/MRU.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
 public class MRU {
-    public MRU() {
-    }
-
     public static void main(String[] args) {
+        float S, So, V, T;
+        
         Scanner leituraTeclado = new Scanner(System.in);
+        
         System.out.println("Insira o valor do espaço inicial (So): ");
         float So = leituraTeclado.nextFloat();
+        
         System.out.println("Insira o valor da velocidade (V): ");
         float V = leituraTeclado.nextFloat();
+        
         System.out.println("Insira o valor do tempo (T): ");
         float T = leituraTeclado.nextFloat();
+        
         float S = So + V * T;
+        
         System.out.println("Os valores iniciais foram: So = " + So + ", V = " + V + " e T = " + T);
         System.out.println("O valor do deslocamento é igual a  " + S);
     }
