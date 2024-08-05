167dc0657de182b16f3606e86a09c0da9501efb5
Linttreen <150390683+Linttreen@users.noreply.github.com>
2024-08-05 09:53:22 -0300

    Update Medida.java

--- src/Medida.java
+++ src/Medida.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
 public class Medida {
-    public Medida() {
-    }
-
     public static void main(String[] args) {
+        float metros, centimetros;
+        
         Scanner leituraTeclado = new Scanner(System.in);
+        
         System.out.println("Insira o valor da medida em metros:");
         float metros = leituraTeclado.nextFloat();
+        
         float centimetros = metros * 100.0F;
+        
         System.out.println("O valor que você inseriu foi de " + metros + " metros");
         System.out.println("O valor em Centímetros: " + centimetros + " cm");
     }
