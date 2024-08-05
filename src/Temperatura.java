3d2903609d1eeaf7671faafc1a33dad651b9e26d
Linttreen <150390683+Linttreen@users.noreply.github.com>
2024-08-05 09:56:26 -0300

    Update Temperatura.java

--- src/Temperatura.java
+++ src/Temperatura.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
 public class Temperatura {
-    public Temperatura() {
-    }
-
     public static void main(String[] args) {
+        float celsius, fahrenheit, kelvin;
+        
         Scanner leituraTeclado = new Scanner(System.in);
+        
         System.out.println("Insira o valor da temperatura em Graus Celsius:");
         float celsius = leituraTeclado.nextFloat();
+        
         float fahrenheit = (9.0F * celsius + 160.0F) / 5.0F;
         float kelvin = celsius + 273.0F;
+        
         System.out.println("O valor que você inseriu foi " + celsius + "°C");
         System.out.println("O valor em Fahrenheit: " + fahrenheit + "°F");
         System.out.println("O valor em Kelvin:" + kelvin + "°K");
