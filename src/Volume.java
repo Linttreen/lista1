c3019e82c192483266ef8052e6e23d381f11a293
Linttreen <150390683+Linttreen@users.noreply.github.com>
2024-08-05 09:57:24 -0300

    Update Volume.java

--- src/Volume.java
+++ src/Volume.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
 public class Volume {
-    public Volume() {
-    }
-
     public static void main(String[] args) {
+        float volume, altura, largura, comprimento;
+        
         Scanner leituraTeclado = new Scanner(System.in);
+        
         System.out.println("Insira o valor da altura em metros:");
         float altura = leituraTeclado.nextFloat();
+        
         System.out.println("Insira o valor da largura em metros:");
         float largura = leituraTeclado.nextFloat();
+        
         System.out.println("Insira o valor do comprimento em metros:");
         float comprimento = leituraTeclado.nextFloat();
+        
         float volume = altura * largura * comprimento;
+        
         System.out.println("O valor que você inseriu foi de " + altura + " m de altura, " + largura + " m de largura e " + comprimento + " m de comprimento.");
         System.out.println("O valor do volume é de: " + volume);
     }
