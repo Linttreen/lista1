dc72f1a9e434d720bb282784bf6eda086c9d9c4f
Linttreen <150390683+Linttreen@users.noreply.github.com>
2024-08-05 09:55:37 -0300

    Update Reajuste.java

--- src/Reajuste.java
+++ src/Reajuste.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
 public class Reajuste {
-    public Reajuste() {
-    }
-
     public static void main(String[] args) {
+        float salario, reajuste;
+        
         Scanner leituraTeclado = new Scanner(System.in);
+        
         System.out.println("Insira o valor do salário:");
         float salario = leituraTeclado.nextFloat();
+        
         System.out.println("O valor do reajuste será de 10%");
+        
         float reajuste = salario + salario * 10.0F / 100.0F;
+        
         System.out.println("O valor inicial do salário era de " + salario);
         System.out.println("O valor do salário reajustado é de " + reajuste);
     }
