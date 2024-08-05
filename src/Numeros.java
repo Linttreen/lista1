c8f04d5da4dcc6c2b59c899443a46beb0c5720e2
Linttreen <150390683+Linttreen@users.noreply.github.com>
2024-08-05 09:54:17 -0300

    Update Numeros.java

--- src/Numeros.java
+++ src/Numeros.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
 public class Numeros {
-    public Numeros() {
-    }
-
     public static void main(String[] args) {
+        int numero, antecessor, sucessor;
+        
         Scanner leituraTeclado = new Scanner(System.in);
+        
         System.out.println("Insira o valor do número: ");
         int numero = leituraTeclado.nextInt();
+        
         int antecessor = numero - 1;
         int sucessor = numero + 1;
+        
         System.out.println("O valor do número inicial foi de " + numero);
         System.out.println("O seu antecessor é " + antecessor + " e seu sucessor é " + sucessor);
     }
